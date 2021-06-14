232eaec6ecc9dc4b78b2e00bab3de614a9f86afd
LanChi <lpham2525@gmail.com>
2021-06-14 12:24:52 -0400

    added more rebels into directory

--- src/App.js
+++ src/App.js
@@ -5,76 +5,106 @@ import Form from './components/Form'
 import Jumbotron from './components/Jumbotron'
 
 let employees = [{
-  id: 1,
+    id: 1,
+    first_name: 'R2-D2',
+    last_name: null,
+    email: 'r2d2@astromech.com',
+    Job_Title: 'Robot',
+    home_world: 'Naboo'
+  },
+  {
+    id: 2,
+    first_name: 'C-3PO',
+    last_name: null,
+    email: 'c3po@protocol.com',
+    Job_Title: 'Robot',
+    home_world: 'Tatooine',
+  },
+  {
+  id: 3,
   first_name: 'Luke',
   last_name: 'Skywalker',
   email: 'luke.skywalker@tatooine.com',
   Job_Title: 'Padawan',
   home_world: 'Tatooine'
-}, {
-  id: 2,
+  }, 
+{
+  id: 4,
   first_name: 'Leia',
   last_name: 'Organa',
   email: 'leia.organa@alderaan.com',
   Job_Title: 'Padawan',
   home_world: 'Alderaan'
-}, {
-  id: 3,
-  first_name: 'Mace',
-  last_name: 'Windu',
-  email: 'mace.windu@haruunkal.com',
-  Job_Title: 'Jedi Master',
-  home_world: 'Haruun Kal'
-}, {
-  id: 4,
+},
+{
+  id: 5,
+  first_name: 'Han',
+  last_name: 'Solo',
+  email: 'han.solo@corellia.com',
+  Job_Title: 'Smuggler',
+  home_world: 'Corellia'
+},
+  {
+    id: 6,
+    first_name: 'Chewbacca',
+    last_name: null,
+    email: 'chewbacca@kashyyyk.com',
+    Job_Title: 'Smuggler',
+    home_world: "Kashyyyk",
+  },
+  {
+    id: 7,
+    first_name: 'Anakin',
+    last_name: 'Skywalker',
+    email: 'anakin.skywalker@podracer.com',
+    Job_Title: 'Padawan',
+    home_world: 'Dagobah'
+  },
+{
+  id: 8,
   first_name: 'Baby',
   last_name: 'Yoda',
   email: 'emailwhatis@grandmaster.com',
   Job_Title: 'Jedi Master',
   home_world: 'unknown'
 }, {
-  id: 5,
-  first_name: 'Ben',
+  id: 9,
+  first_name: 'Obi-Wan',
   last_name: 'Kenobi',
-  email: 'obiwan.kenobi@stewjon.com',
+  email: 'ben.kenobi@stewjon.com',
   Job_Title: 'Jedi Master',
   home_world: 'Stewjon'
-}, {
-  id: 6,
+}, 
+  {
+    id: 10,
+    first_name: 'Lando',
+    last_name: 'Calrissian',
+    email: 'lando.calrissian@socorro.com',
+    Job_Title: 'Smuggler',
+    home_world: 'Socorro'
+  },{
+  id: 11,
   first_name: 'Qui-Gon',
   last_name: 'Jinn',
   email: 'quigon.jinn@coruscant.com',
   Job_Title: 'Jedi Master',
   home_world: 'Coruscant'
 }, {
-  id: 7,
+  id: 12,
   first_name: 'Rey',
   last_name: 'Skywalker',
   email: 'rey.skywalker@jakku.com',
   Job_Title: 'Padawan',
   home_world: 'Jakku'
 }, {
-  id: 8,
+  id:13,
   first_name: 'Ben',
   last_name: 'Solo',
   email: 'ben.solo@chandrila.com',
   Job_Title: 'Padawan',
   home_world: 'Chandrila'
-}, {
-  id: 9,
-  first_name: 'Anakin',
-  last_name: 'Skywalker',
-  email: 'anakin.skywalker@podracer.com',
-  Job_Title: 'Padawan',
-  home_world: 'Dagobah'
-}, {
-  id: 10,
-  first_name: 'Ezra',
-  last_name: 'Bridger',
-  email: 'ezra.bridger@lothal.com',
-  Job_Title: 'Padawan',
-  home_world: "Lothal"
-}]
+}
+]
 
 class App extends Component {
 
@@ -85,7 +115,7 @@ class App extends Component {
     firstName: 'first_name'
   }
 
-  handleSortByRank = () => {
+  handleSortByJob = () => {
     let employees = this.state.employees
     employees.sort((a, b) => {
       let master = a.Job_Title
@@ -166,12 +196,12 @@ class App extends Component {
   }
 
   handleReset = () => {
-    let reset = employees.sort((a,b) => {
-      return a.id-b.id
+    let reset = employees.sort((a, b) => {
+      return a.id - b.id
     })
-      this.setState({
+    this.setState({
       employees: reset, inputValue: ''
-      })
+    })
   }
 
   render() {
@@ -187,14 +217,14 @@ class App extends Component {
               handleInputChange={this.handleInputChange}
               handleSearch={this.handleSearch}
               handleFirst={this.handleFirst} />
-              <p>
-            <button onClick={this.handleSortFirstName} className="btn btn-primary">Sort by first name</button>
+            <p>
+              <button onClick={this.handleSortFirstName} className="btn btn-primary">Sort by first name</button>
 
-            <button onClick={this.handleSortLastName} className="btn btn-success">Sort by last name</button>
+              <button onClick={this.handleSortLastName} className="btn btn-success">Sort by last name</button>
 
-            <button onClick={this.handleSortByRank} className="btn btn-secondary">Sort by rank</button>
+              <button onClick={this.handleSortByJob} className="btn btn-secondary">Sort by occupation</button>
 
-            <button onClick={this.handleReset} className="btn btn-danger">Reset list</button>
+              <button onClick={this.handleReset} className="btn btn-danger">Reset list</button>
             </p>
             <Employee employees={this.state.employees} />
           </div>
